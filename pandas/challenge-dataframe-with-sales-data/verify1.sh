0b0a0429d4dfa12f2782f12ff37a6234f45d8d70
17395 <invulnerableandre@gmail.com>
2023-05-13 18:19:51 -0500

    verify1.sh:update

--- pandas/challenge-dataframe-with-sales-data/verify1.sh
+++ pandas/challenge-dataframe-with-sales-data/verify1.sh
@@ -14,5 +14,5 @@ if  grep -q "df.dropna()" "/home/labex/project/data_cleaning.py" ; then
         echo 'No reset_index() detected';
     fi 
 else
-    echo 'No dropna() detected'; 
+    exit 0; 
 fi
