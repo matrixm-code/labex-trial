b7531379544989ff3b8aeab4b31545a049abaed0
17395 <invulnerableandre@gmail.com>
2023-05-13 21:45:36 -0500

    verify2.sh: test

--- pandas/challenge-dataframe-with-sales-data/verify2.sh
+++ pandas/challenge-dataframe-with-sales-data/verify2.sh
@@ -1,14 +1,15 @@
 #!/bin/zsh
-if  grep -q ".dt.year" "/home/labex/project/feature_engineering.py" ; then
-    if grep -q ".dt.month" "/home/labex/project/feature_engineering.py"; then
-        if grep -q ".dt.day" "/home/labex/project/feature_engineering.py"; then
-            cd /tmp && python3 test_feature_engineering.py
-        else
-            exit 1;
-        fi
-    else
-        exit 1;
-    fi
-else
-    exit 1;
-fi
+# if  grep -q ".dt.year" "/home/labex/project/feature_engineering.py" ; then
+#     if grep -q ".dt.month" "/home/labex/project/feature_engineering.py"; then
+#         if grep -q ".dt.day" "/home/labex/project/feature_engineering.py"; then
+#             cd /tmp && python3 test_feature_engineering.py
+#         else
+#             exit 1;
+#         fi
+#     else
+#         exit 1;
+#     fi
+# else
+#     exit 1;
+# fi
+cd /tmp && python3 test_feature_engineering.py
