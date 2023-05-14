f0b9dc9f56b5b118841dafda81394acf7c54934a
17395 <invulnerableandre@gmail.com>
2023-05-13 22:40:03 -0500

    verify4.sh: test

--- pandas/challenge-dataframe-with-sales-data/verify4.sh
+++ pandas/challenge-dataframe-with-sales-data/verify4.sh
@@ -2,19 +2,19 @@
 if  grep -q "plt.bar(" "/home/labex/project/data_visualization.py" ; then
     if  grep -q "plt.plot" "/home/labex/project/data_visualization.py" && grep -q "plt.legend(" "/home/labex/project/data_visualization.py" ; then
         if grep -q "plt.matshow" "/home/labex/project/data_visualization.py" && grep -q "plt.colorbar" "/home/labex/project/data_visualization.py"; then
-            # if grep -q "plt.pie" "/home/labex/project/data_visualization.py"; then
-            #     if grep -q "plt.xlabel" "/home/labex/project/data_visualization.py" && grep -q "plt.ylabel" "/home/labex/project/data_visualization.py" && grep -q "plt.title" "/home/labex/project/data_visualization.py"; then
-            #         if grep -q "plt.show()" "/home/labex/project/data_visualization.py"; then
-            #             cd /tmp && python3 test_data_visualization.py
-            #         else
-            #             exit 1;
-            #         fi
-            #     else
-            #         exit 1;
-            #     fi
-            # else
-            #     exit 1;
-            # fi
+            if grep -q "plt.pie" "/home/labex/project/data_visualization.py"; then
+                if grep -q "plt.xlabel" "/home/labex/project/data_visualization.py" && grep -q "plt.ylabel" "/home/labex/project/data_visualization.py" && grep -q "plt.title" "/home/labex/project/data_visualization.py"; then
+                    if grep -q "plt.show()" "/home/labex/project/data_visualization.py"; then
+                        cd /tmp && python3 test_data_visualization.py
+                    else
+                        exit 1;
+                    fi
+                else
+                    exit 1;
+                fi
+            else
+                exit 1;
+            fi
         else
             exit 1;
         fi
