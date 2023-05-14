10b4743215b1c6d294ee5c88d06c4a99cd9155bd
17395 <invulnerableandre@gmail.com>
2023-05-13 22:21:35 -0500

    verify3.sh: test

--- pandas/challenge-dataframe-with-sales-data/verify4.sh
+++ pandas/challenge-dataframe-with-sales-data/verify4.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 if  grep -q "plt.bar(" "/home/labex/project/data_visualization.py" ; then
-    if  grep "plt.plot/|plt.legend(" "/home/labex/project/data_visualization.py" ; then
-        if grep "plt.matshow/|plt.colorbar" "/home/labex/project/data_visualization.py"; then
+    if  grep -q "plt.plot" "/home/labex/project/data_visualization.py" && grep -q "plt.legend(" "/home/labex/project/data_visualization.py" ; then
+        if grep -q "plt.matshowr" "/home/labex/project/data_visualization.py" && grep -q "plt.colorba" "/home/labex/project/data_visualization.py"; then
             if grep -q "plt.pie" "/home/labex/project/data_visualization.py"; then
-                if grep "plt.xlabel/|plt.ylabel/|plt.title" "/home/labex/project/data_visualization.py"; then
+                if grep -q "plt.xlabel" "/home/labex/project/data_visualization.py" && grep -q "plt.ylabel" "/home/labex/project/data_visualization.py" && grep -q "plt.title" "/home/labex/project/data_visualization.py"; then
                     if grep -q "plt.show()" "/home/labex/project/data_visualization.py"; then
                         cd /tmp && python3 test_data_visualization.py
                     else
