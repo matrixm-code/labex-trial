cb8582d3ac2e1639f0b057120e1dec50c2f0ecea
17395 <invulnerableandre@gmail.com>
2023-05-13 21:26:11 -0500

    verify2.sh: test

--- pandas/challenge-dataframe-with-sales-data/verify2.sh
+++ pandas/challenge-dataframe-with-sales-data/verify2.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
-if  grep "df['Date'].dt.year" "/home/labex/project/feature_engineering.py" ; then
-    if grep "df['Date'].dt.month" "/home/labex/project/feature_engineering.py"; then
-        if grep "df['Date'].dt.day'" "/home/labex/project/feature_engineering.py"; then
+if  grep -q "df['Date'].dt.year" "/home/labex/project/feature_engineering.py" ; then
+    if grep -q "df['Date'].dt.month" "/home/labex/project/feature_engineering.py"; then
+        if grep -q "df['Date'].dt.day'" "/home/labex/project/feature_engineering.py"; then
             cd /tmp && python3 test_feature_engineering.py
         else
             exit 1
