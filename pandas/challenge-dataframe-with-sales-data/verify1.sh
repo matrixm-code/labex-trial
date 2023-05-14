35e8a8ca80b5b28be37fb3c67b22849a6963bc51
17395 <invulnerableandre@gmail.com>
2023-05-13 20:50:38 -0500

    verify1.sh: test

--- pandas/challenge-dataframe-with-sales-data/verify1.sh
+++ pandas/challenge-dataframe-with-sales-data/verify1.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 if  grep -q "df.dropna()" "/home/labex/project/data_cleaning.py" ; then
     if grep -q ".reset_index(" "/home/labex/project/data_cleaning.py"; then
-        if grep -q "pd.to_datetime(df['Date'])" "/home/labex/project/data_cleaning.py"; then
+        if grep -q "pd.to_datetime(" "/home/labex/project/data_cleaning.py"; then
             if grep -q "df.drop_duplicates(" "/home/labex/project/data_cleaning.py"; then
                 cd /tmp && python3 test_data_cleaning.py
             else
                 exit 1;
             fi
         else
-            exit 0;
+            exit 1;
         fi
     else
         exit 1;
--- pandas/challenge-dataframe-with-sales-data/verify2.sh
+++ pandas/challenge-dataframe-with-sales-data/verify2.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-if  grep -q "df['Date'].dt.year/|df['Date'].dt.month/|df['Date'].dt.day" "/home/labex/project/future_engineering.py" ; then
+if  grep -q "df['Date'].dt.year/|df['Date'].dt.month/|df['Date'].dt.day" "/home/labex/project/feature_engineering.py" ; then
     cd /tmp && python3 test_feature_engineering.py
 else
     exit 1
