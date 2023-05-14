4dd80476e844c908f875c8e421156faf2bda4afe
17395 <invulnerableandre@gmail.com>
2023-05-13 22:13:58 -0500

    verify3.sh: test

--- pandas/challenge-dataframe-with-sales-data/verify3.sh
+++ pandas/challenge-dataframe-with-sales-data/verify3.sh
@@ -1,18 +1,18 @@
 #!/bin/zsh
 if  grep -q ".sum()" "/home/labex/project/data_aggregation.py" ; then
-    # if grep -q ".mean()" "/home/labex/project/data_aggregation.py"; then
-    #     if grep -q ".nlargest(10" "/home/labex/project/data_aggregation.py"; then
-    #         if grep -q ".loc[" "/home/labex/project/data_aggregation.py"; then
-    #             cd /tmp && python3 test_data_aggregation.py
-    #         else
-    #             exit 1;
-    #         fi
-    #     else
-    #         exit 1;
-    #     fi
-    # else
-    #     exit 1;
-    # fi 
+    if grep -q ".mean()" "/home/labex/project/data_aggregation.py"; then
+        # if grep -q ".nlargest(10" "/home/labex/project/data_aggregation.py"; then
+        #     if grep -q ".loc[" "/home/labex/project/data_aggregation.py"; then
+        #         cd /tmp && python3 test_data_aggregation.py
+        #     else
+        #         exit 1;
+        #     fi
+        # else
+        #     exit 1;
+        # fi
+    else
+        exit 1;
+    fi 
 else
     exit 1;
 fi
