4863aa130175a5c2d1184bafe2203d0c05c61176
17395 <invulnerableandre@gmail.com>
2023-05-16 11:34:30 -0500

    asset: test return value

--- pandas/challenge-dataframe-with-sales-data/assets/test_data_cleaning.py
+++ pandas/challenge-dataframe-with-sales-data/assets/test_data_cleaning.py
@@ -36,23 +36,23 @@ class TestAdvancedPandasChallenge(unittest.TestCase):
                             '2021-06-06', '2021-06-07', '2021-07-08', '2021-07-10', '2021-07-10',
                             '2021-07-11', '2021-07-11', '2021-09-14', '2021-10-15',
                             '2021-10-16', '2021-11-17'],
-                    'Product': ['Product A', 'Product B', 'Product C', 'Product C', 'Product E',
-                                'Product F', 'Product G', 'Product H', 'Product C', 'Product J',
-                                'Product K', 'Product N', 'Product N', 'Product O',
-                                'Product P', 'Product Q'],
-                    'Category': ['Electronics', 'Electronics', 'Home', 'Fashion', 'Fashion',
-                                'Home', 'Electronics', 'Fashion', 'Home', 'Electronics',
-                                'Fashion', 'Electronics', 'Fashion', 'Home',
-                                'Electronics', 'Fashion'],
-                    'Price': [300, 150, 60, 40, 80, 90, 200, 50, 100, 350, 120, 400, 60, 70, 250, 100],
-                    'Items Sold': [10, 7, 20, 15, 12, 8, 6, 14, 9, 4, 10, 5, 16, 13, 7, 9]}
-        expected_df = pd.DataFrame(expected_data, columns=['Date'])
-        expected_df = expected_df.squeeze()
+                        'Product': ['Product A', 'Product B', 'Product C', 'Product C', 'Product E',
+                                    'Product F', 'Product G', 'Product H', 'Product C', 'Product J',
+                                    'Product K', 'Product N', 'Product N', 'Product O',
+                                    'Product P', 'Product Q'],
+                        'Category': ['Electronics', 'Electronics', 'Home', 'Fashion', 'Fashion',
+                                    'Home', 'Electronics', 'Fashion', 'Home', 'Electronics',
+                                    'Fashion', 'Electronics', 'Fashion', 'Home',
+                                    'Electronics', 'Fashion'],
+                        'Price': [300, 150, 60, 40, 80, 90, 200, 50, 100, 350, 120, 400, 60, 70, 250, 100],
+                        'Items Sold': [10, 7, 20, 15, 12, 8, 6, 14, 9, 4, 10, 5, 16, 13, 7, 9]}
+        expected_df = pd.DataFrame(expected_data)
+        expected_df['Date']=pd.to_datetime(expected_df['Date'])
         cleaned_df = clean_data(self.df)
-        cleaned_df = pd.DataFrame(cleaned_df, columns=['Date'])
-        cleaned_df = cleaned_df.squeeze()
-        
-        pd.testing.assert_frame_equal(cleaned_df, cleaned_df, check_dtype=False)
+        cleaned_df = pd.DataFrame(cleaned_df)
+        pd.testing.assert_frame_equal(cleaned_data.reset_index(drop=True), expected_df.reset_index(drop=True), check_dtype=False)
+
+        # expected_df.equals(cleaned_df)
         # pd.testing.assert_frame_equal(expected_data["Date"], cleaned_df["Date"])
         # pd.testing.assert_frame_equal(expected_data["Product"], cleaned_df["Product"])
 
