7d6a4a74204788754807268173de6fc351c49d81
17395 <invulnerableandre@gmail.com>
2023-05-16 00:08:15 -0500

    asset: update DataFrame in tests

--- pandas/challenge-dataframe-with-sales-data/assets/test_data_visualization.py
+++ pandas/challenge-dataframe-with-sales-data/assets/test_data_visualization.py
@@ -13,20 +13,20 @@ from data_visualization import visualize_data
 class TestAdvancedPandasChallenge(unittest.TestCase):
 
     def setUp(self):
-        self.data = {'Date': ['2021-03-01', '2021-03-02', '2021-03-03', '2021-03-04', '2021-03-05',
-                              '2021-03-06', '2021-03-07', '2021-03-08', '2021-03-09', '2021-03-10',
-                              '2021-03-11', '2021-03-12', '2021-03-13', '2021-03-14', '2021-03-15',
-                              '2021-03-16', '2021-03-17', '2021-03-18', '2021-03-19', '2021-03-20'],
-                     'Product': ['Product A', 'Product B', 'Product C', 'Product D', 'Product E',
-                                 'Product F', 'Product G', 'Product H', 'Product I', 'Product J',
-                                 'Product K', 'Product L', 'Product M', 'Product N', 'Product O',
-                                 'Product P', 'Product Q', 'Product R', 'Product S', 'Product T'],
-                     'Category': ['Electronics', 'Electronics', 'Home', 'Fashion', 'Fashion',
-                                  'Home', 'Electronics', 'Fashion', 'Home', 'Electronics',
-                                  'Fashion', 'Home', 'Electronics', 'Fashion', 'Home',
-                                  'Electronics', 'Fashion', 'Home', 'Electronics', 'Fashion'],
-                     'Price': [300, 150, 60, 40, 80, 90, 200, 50, 100, 350, 120, 80, 400, 60, 70, 250, 100, 110, 180, 90],
-                     'Items Sold': [10, 7, 20, 15, 12, 8, 6, 14, 9, 4, 10, 11, 5, 16, 13, 7, 9, 12, 8, 11]}
+        self.data = {'Date': ['2021-03-01', '2021-03-02', '2021-03-03', '2021-05-04', '2021-05-05',
+                    '2021-06-06', '2021-06-07', '2021-07-08', '2021-07-10', '2021-07-10',
+                    '2021-07-11', '2021-07-11', '2021-07-11', '2021-09-14', '2021-10-15',
+                    '2021-10-16', '2021-11-17', '2021-12-18', '2021-12-19', np.nan],
+                    'Product': ['Product A', 'Product B', 'Product C', 'Product C', 'Product E',
+                                'Product F', 'Product G', 'Product H', 'Product C', 'Product J',
+                                'Product K', 'Product N', 'Product N', 'Product N', 'Product O',
+                                'Product P', 'Product Q', np.nan, np.nan, 'Product T'],
+                    'Category': ['Electronics', 'Electronics', 'Home', 'Fashion', 'Fashion',
+                                'Home', 'Electronics', 'Fashion', 'Home', 'Electronics',
+                                'Fashion', 'Electronics', 'Electronics', 'Fashion', 'Home',
+                                'Electronics', 'Fashion', 'Home', 'Electronics', 'Fashion'],
+                    'Price': [300, 150, 60, 40, 80, 90, 200, 50, 100, 350, 120, 400, 400, 60, 70, 250, 100, 110, 180, 90],
+                    'Items Sold': [10, 7, 20, 15, 12, 8, 6, 14, 9, 4, 10, 5, 5, 16, 13, 7, 9, np.nan, 8, 11]}
 
         self.df = pd.DataFrame(self.data)
 
