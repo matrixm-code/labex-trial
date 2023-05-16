28ac209f3606f33de3271787e08d7fa611569162
17395 <invulnerableandre@gmail.com>
2023-05-16 00:33:48 -0500

    asset: tests

--- pandas/challenge-dataframe-with-sales-data/assets/test_data_cleaning.py
+++ pandas/challenge-dataframe-with-sales-data/assets/test_data_cleaning.py
@@ -31,8 +31,8 @@ class TestAdvancedPandasChallenge(unittest.TestCase):
         cleaned_df = clean_data(self.df)
         self.assertTrue(isinstance(cleaned_df, pd.DataFrame))
 
-if __name__ == '__main__':
-    expected_data = {'Date': ['2021-03-01', '2021-03-02', '2021-03-03', '2021-05-04', '2021-05-05',
+    def test_result(self):
+        expected_data = {'Date': ['2021-03-01', '2021-03-02', '2021-03-03', '2021-05-04', '2021-05-05',
                             '2021-06-06', '2021-06-07', '2021-07-08', '2021-07-10', '2021-07-10',
                             '2021-07-11', '2021-07-11', '2021-09-14', '2021-10-15',
                             '2021-10-16', '2021-11-17'],
@@ -46,10 +46,15 @@ if __name__ == '__main__':
                                 'Electronics', 'Fashion'],
                     'Price': [300, 150, 60, 40, 80, 90, 200, 50, 100, 350, 120, 400, 60, 70, 250, 100, 110, 180, 90],
                     'Items Sold': [10, 7, 20, 15, 12, 8, 6, 14, 9, 4, 10, 5, 16, 13, 7, 9]}
-    cleaned_df = clean_data(self.df)
-    self.assertTrue(pd.testing.assert_series_equal(expected_data["Date"], cleaned_df["Date"]))
-    self.assertTrue(pd.testing.assert_series_equal(expected_data["Product"], cleaned_df["Product"]))
-    self.assertTrue(pd.testing.assert_series_equal(expected_data["Category"], cleaned_df["Category"]))
-    self.assertTrue(pd.testing.assert_series_equal(expected_data["Price"], cleaned_df["Price"]))
-    self.assertTrue(pd.testing.assert_series_equal(expected_data["Items Sold"], cleaned_df["Items Sold"]))
+        cleaned_df = clean_data(self.df)
+        self.assertTrue(pd.testing.assert_frame_equal(cleaned_df, expected_data))
+
+if __name__ == '__main__':
+    
+    # cleaned_df = clean_data(self.df)
+    # self.assertTrue(pd.testing.assert_series_equal(expected_data["Date"], cleaned_df["Date"]))
+    # self.assertTrue(pd.testing.assert_series_equal(expected_data["Product"], cleaned_df["Product"]))
+    # self.assertTrue(pd.testing.assert_series_equal(expected_data["Category"], cleaned_df["Category"]))
+    # self.assertTrue(pd.testing.assert_series_equal(expected_data["Price"], cleaned_df["Price"]))
+    # self.assertTrue(pd.testing.assert_series_equal(expected_data["Items Sold"], cleaned_df["Items Sold"]))
     unittest.main()
