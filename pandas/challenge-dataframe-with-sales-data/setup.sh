448818bc89c16cb67e327c8e39514d96d24dc390
17395 <invulnerableandre@gmail.com>
2023-05-16 10:58:45 -0500

    asset: tests

--- pandas/challenge-dataframe-with-sales-data/setup.sh
+++ pandas/challenge-dataframe-with-sales-data/setup.sh
@@ -1,15 +1,3 @@
 #!/bin/zsh
-# pip install pandas/dataframe
-# pip install pandas/drop_duplicates
-# pip install pandas/math_methods
-# pip install pandas/comparison_methods
-# pip install pandas/dt
-# pip install pandas/apply
-# pip install pandas/groupby
-# pip install pandas/loc
-# pip install matplotlib/pie
-# pip install matplotlib/bar
-# pip install matplotlib/plot
-# pip install matplotlib/matshow
 pip install pandas==2.0.1
 pip install matplotlib==3.7.1
