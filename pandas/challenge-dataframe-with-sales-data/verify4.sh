ef0655009d787d81b7bdfba58eb71f061d39894f
17395 <invulnerableandre@gmail.com>
2023-05-17 17:21:07 -0500

    verify4.sh:update the limit to use matploit

--- pandas/challenge-dataframe-with-sales-data/verify4.sh
+++ pandas/challenge-dataframe-with-sales-data/verify4.sh
@@ -5,7 +5,7 @@ if  grep -q ".bar(" "/home/labex/project/data_visualization.py" ; then
             if grep -q ".pie" "/home/labex/project/data_visualization.py"; then
                 if grep -q ".xlabel" "/home/labex/project/data_visualization.py" && grep -q ".ylabel" "/home/labex/project/data_visualization.py" && grep -q ".title" "/home/labex/project/data_visualization.py"; then
                     if grep -q ".show()" "/home/labex/project/data_visualization.py"; then
-                        
+                        cd /tmp && python3 test_data_visualization.py
                     else
                         exit 1;
                     fi
