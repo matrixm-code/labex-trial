83baa7a8ab23e6c0b1440060457bcbb6e6d6d528
huhuhang <huhuhang@users.noreply.github.com>
2023-05-22 01:29:12 +0000

    Formatting documents

--- linux/challenge-path-count/verify1.sh
+++ linux/challenge-path-count/verify1.sh
@@ -3,8 +3,8 @@
 cd ~/project
 a=$(echo $PATH | sed 's/:/\n/g' | wc -l)
 b=$(bash pathCount.sh 2> /dev/null | grep "\/" | wc -l)
-if [[ $a -eq $b ]];then
-	exit 0
+if [[ $a -eq $b ]]; then
+  exit 0
 else
-	exit 1
+  exit 1
 fi
