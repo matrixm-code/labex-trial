7c09dc7141e6d15da00cbbf57abeee5920d97322
huhuhang <huhuhang@users.noreply.github.com>
2023-05-22 01:26:53 +0000

    Formatting documents

--- linux/challenge-print-lines/verify1.sh
+++ linux/challenge-print-lines/verify1.sh
@@ -3,8 +3,8 @@
 cd ~/project
 a=$(bash printLines.sh 1 2 inputFile.txt | head -1)
 b=$(bash printLines.sh 1 2 inputFile.txt | tail -n 1)
-if [[ $a == "is" && $b == "my" ]];then
-	exit 0
+if [[ $a == "is" && $b == "my" ]]; then
+  exit 0
 else
-	exit 1
+  exit 1
 fi
