cd1042a788b2f9bb3ae5d86c3b70c26b0d7b124e
huhuhang <huhuhang@users.noreply.github.com>
2023-05-22 01:22:22 +0000

    Formatting documents

--- linux/challenge-recursive-function/verify1.sh
+++ linux/challenge-recursive-function/verify1.sh
@@ -7,8 +7,8 @@ bash recFunction.sh 12 a bb | grep -q "a$"
 rs1=$?
 bash recFunction.sh 12 a bb | grep -q "bb$"
 rs2=$?
-if [[ $rs -eq 0 && $rs1 -eq 0 && $rs2 -eq 0 ]];then
-	exit 0
+if [[ $rs -eq 0 && $rs1 -eq 0 && $rs2 -eq 0 ]]; then
+  exit 0
 else
-	exit 1
+  exit 1
 fi
