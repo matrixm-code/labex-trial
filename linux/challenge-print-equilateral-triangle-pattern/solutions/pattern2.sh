b3ac6862be2367bb641671999d9163dee4fd4254
huhuhang <huhuhang@users.noreply.github.com>
2023-05-22 01:27:58 +0000

    Formatting documents

--- linux/challenge-print-equilateral-triangle-pattern/solutions/pattern2.sh
+++ linux/challenge-print-equilateral-triangle-pattern/solutions/pattern2.sh
@@ -4,21 +4,16 @@
 echo "[INPUT]Enter  the value of N[INPUT]"
 read inputN
 
-
-if [ $inputN -gt 0 ]
-then
-	count=1
-	echo "[OUTPUT]The Patter is Executed as shown below[OUTPUT]"
-	for(( row=1; row<=$inputN; row++ ))
-	do
-	for(( col=1; col<=row; col++ ))
-	do
-		echo -ne "$count\t"
-		count=$(($count+1))
-	done
-		echo " "
-	done
+if [ $inputN -gt 0 ]; then
+  count=1
+  echo "[OUTPUT]The Patter is Executed as shown below[OUTPUT]"
+  for ((row = 1; row <= $inputN; row++)); do
+    for ((col = 1; col <= row; col++)); do
+      echo -ne "$count\t"
+      count=$(($count + 1))
+    done
+    echo " "
+  done
 else
-	echo "[ERROR]Invalid Input[ERROR]"
+  echo "[ERROR]Invalid Input[ERROR]"
 fi
-
--- linux/challenge-print-equilateral-triangle-pattern/verify1.sh
+++ linux/challenge-print-equilateral-triangle-pattern/verify1.sh
@@ -3,8 +3,8 @@
 cd ~/project
 echo -e "4\n" | bash pattern2.sh > /tmp/output.txt
 rs=$(cat /tmp/output.txt | tail -n 1 | awk '{print $4}')
-if [[ $rs -eq 10 ]];then
-	exit 0
+if [[ $rs -eq 10 ]]; then
+  exit 0
 else
-	exit 1
+  exit 1
 fi
